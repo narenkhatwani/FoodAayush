9c8588b616f3806b9fb98d323e79599a12f3c94b
narenkhatwani <48763124+narenkhatwani@users.noreply.github.com>
2020-08-09 17:34:21 +0530

    Updated streamlit with sidebar and title
    Title and sidebar with some info added

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,8 +1,15 @@
 import streamlit as st
+
 #https://docs.streamlit.io/en/stable/api.html#display-text
 #for widget types 
+
 
 # adding title
 st.title('Food Aayush')
 # adding text    
 st.markdown('Food Quality Analysis at your fingertips')
+
+#declare and add sidebar
+st.sidebar.title("Menu")
+
+st.sidebar.info("Welcome to Food Aayush Data Analytics. Here you can analyse the nutritional value of food and draw some schematics")
