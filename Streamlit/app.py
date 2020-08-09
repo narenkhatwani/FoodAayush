03f1094479fa7ad92f7609bd50f54894db3d554e
narenkhatwani <48763124+narenkhatwani@users.noreply.github.com>
2020-08-09 17:09:18 +0530

    Initialized app.py

---
+++ Streamlit/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+#https://docs.streamlit.io/en/stable/api.html#display-text
+#for widget types 
+
+# adding title
+st.title('Food Aayush')
+# adding text    
+st.markdown('Food Quality Analysis at your fingertips')
