94682fad2002c716c792e0a65bf9080df576db2a
narenkhatwani <48763124+narenkhatwani@users.noreply.github.com>
2020-08-10 12:28:37 +0530

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -17,10 +17,12 @@ st.title('Food Aayush')
 # adding text    
 st.markdown('Food Quality Analysis at your fingertips')
 
-#declare and add sidebar
-st.sidebar.title("Menu")
+
 
 st.sidebar.info("Welcome to Food Aayush Data Analytics. Here you can analyse the nutritional value of food and draw some schematics")
+
+#declare and add sidebar
+st.sidebar.title("Menu")
 
 #read csv file
 DATA_URL = ("resources/assets/01.csv")
