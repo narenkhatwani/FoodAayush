61cf8d51fa95a4f7749d92f7f4d2385f76162013
narenkhatwani <48763124+narenkhatwani@users.noreply.github.com>
2020-08-20 21:33:56 +0530

    New features added
    New features added

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -48,16 +48,19 @@ data2= load_data2()
 
 #to print a small iframe of the csv file 
 #format is 'name displayed above dataset','variable in which csv is loaded'
-'Dataset Preview',data
+
+st.markdown(f"<span style='color: #000080;font-size: 24px;font-weight: bold;'> ->Dataset Preview</span>", unsafe_allow_html=True)
+
+data
 
 subset_data=data
 
 ### MULTISELECT
-food_name_input = st.multiselect('Food name',data.groupby('name').count().reset_index()['name'].tolist())
+#food_name_input = st.multiselect('Food name',data.groupby('name').count().reset_index()['name'].tolist())
 
 # by food name
-if len(food_name_input) > 0:
-    subset_data = data[data['name'].isin(food_name_input)]
+#if len(food_name_input) > 0:
+ #   subset_data = data[data['name'].isin(food_name_input)]
 
 #sidebar title
 st.sidebar.title("Filter data")
@@ -66,9 +69,9 @@ st.sidebar.title("Filter data")
 food_list = st.sidebar.selectbox("Select food name", data["name"].unique())
 
 
-
-st.markdown(f"<span style='color: black;font-size: 24px;font-weight: bold;'>You selected- {food_list}</span>", unsafe_allow_html=True)
-
+st.markdown(f"<span style='color: #000080;font-size: 24px;font-weight: bold;'> ->Filter Data Results</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: black;font-size: 22px;font-weight: bold;'>You selected- {food_list}</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: black;font-size: 22px;font-weight: bold;'>Nutritional Analysis for filtered Data is as follows-</span>", unsafe_allow_html=True)
 
 #counts of various nutritional contents of a food item
 count_water = data.loc[(data["name"] == food_list) , 'water'].iloc[0]
@@ -81,20 +84,21 @@ count_fibresoluble = data.loc[(data["name"] == food_list) , 'fibsol'].iloc[0]
 count_carbohydrate = data.loc[(data["name"] == food_list) , 'choavldf'].iloc[0]
 count_energy = data.loc[(data["name"] == food_list) , 'enerc'].iloc[0]
 
-st.markdown(f"<span style='color: blue;font-size: 24px;font-weight: bold;'>Water- {count_water}g</span>", unsafe_allow_html=True)
-st.markdown(f"<span style='color: blue;font-size: 24px;font-weight: bold;'>Protein- {count_protein}g</span>", unsafe_allow_html=True)
-st.markdown(f"<span style='color: blue;font-size: 24px;font-weight: bold;'>Ash- {count_ash}g</span>", unsafe_allow_html=True)
-st.markdown(f"<span style='color: blue;font-size: 24px;font-weight: bold;'>Fat- {count_fat}g</span>", unsafe_allow_html=True)
-st.markdown(f"<span style='color: blue;font-size: 24px;font-weight: bold;'>Total Fibre- {count_fibretotal}g</span>", unsafe_allow_html=True)
-st.markdown(f"<span style='color: blue;font-size: 24px;font-weight: bold;'>Insoluble Fibre- {count_fibreinsoluble}g</span>", unsafe_allow_html=True)
-st.markdown(f"<span style='color: blue;font-size: 24px;font-weight: bold;'>Soluble Fibre- {count_fibresoluble}g</span>", unsafe_allow_html=True)
-st.markdown(f"<span style='color: blue;font-size: 24px;font-weight: bold;'>Carbohydrates- {count_carbohydrate}g</span>", unsafe_allow_html=True)
-st.markdown(f"<span style='color: blue;font-size: 24px;font-weight: bold;'>Energy- {count_energy}kJ</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Water- {count_water}g</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Protein- {count_protein}g</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Ash- {count_ash}g</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Fat- {count_fat}g</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Total Fibre- {count_fibretotal}g</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Insoluble Fibre- {count_fibreinsoluble}g</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Soluble Fibre- {count_fibresoluble}g</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Carbohydrates- {count_carbohydrate}g</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Energy- {count_energy}kJ</span>", unsafe_allow_html=True)
 
 #sidebar title
 st.sidebar.title("Search for Recipe")
 
-'Dataset Preview',data2
+st.markdown(f"<span style='color: #000080;font-size: 24px;font-weight: bold;'> ->Dataset Preview</span>", unsafe_allow_html=True)
+data2
 
 #Checkbox for Hospitals
 ingredient_1 = st.sidebar.selectbox("Select 1st ingredient name", data2["ing1"].unique())
@@ -105,4 +109,42 @@ ingredient_5 = st.sidebar.selectbox("Select 5th ingredient name", data2["ing5"].
 
 recipe = data2.loc[(data2['ing1'] == ingredient_1) & (data2['ing2'] == ingredient_2) & (data2['ing3'] == ingredient_3) & (data2['ing4'] == ingredient_4) & (data2['ing5'] == ingredient_5), 'name'].iloc[0]
 
-st.markdown(f"<span style='color: black;font-size: 24px;font-weight: bold;'>Possible Dishes- {recipe}</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: black;font-size: 22px;font-weight: bold;'>Possible Dishes- {recipe}</span>", unsafe_allow_html=True)
+
+st.markdown(f"<span style='color: #000080;font-size: 24px;font-weight: bold;'>->Add your total daily intake of calories</span>", unsafe_allow_html=True)
+
+x = st.slider('(in terms of Calories)',0,3000)
+
+
+fat_value= x*(30/100)
+sat_fat_value= x*(7/100)
+trans_fat_value= x*(1/100)
+total_carbs_value= x*(50/100)
+protein_value= x*(20/100)
+
+
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Total Fat content should be- {fat_value} Cal</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Saturated Fat count should be- {sat_fat_value} Cal</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Trans Fat count should be- {trans_fat_value} Cal</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Total Carbohydrates count should be- {total_carbs_value} Cal</span>", unsafe_allow_html=True)
+st.markdown(f"<span style='color: blue;font-size: 22px;font-weight: bold;'>Protein count should be- {protein_value} Cal</span>", unsafe_allow_html=True)
+
+
+st.markdown(f"<span style='color: #000080;font-size: 24px;font-weight: bold;'>->Want to know your ideal calorie intake ??</span>", unsafe_allow_html=True)
+
+st.markdown(f"<span style='color: black;font-size: 20px;font-weight: bold;'>Choose your gender</span>", unsafe_allow_html=True)
+gender = st.selectbox('*Your calorie intake depends on your gender',('Male', 'Female', 'Other','Rather Not Say'))
+
+st.markdown(f"<span style='color: black;font-size: 20px;font-weight: bold;'>You selected: {gender}</span>", unsafe_allow_html=True)
+
+if (gender == 'Male'):
+    st.markdown(f"<span style='color: blue;font-size: 18px;font-weight: bold;'>Your ideal daily calorie intake should be 2500 Cal</span>", unsafe_allow_html=True)
+    st.markdown(f"<span style='color: blue;font-size: 18px;font-weight: bold;'>Your ideal daily water intake should be 3.7 L</span>", unsafe_allow_html=True)
+elif (gender == 'Female'):
+    st.markdown(f"<span style='color: blue;font-size: 18px;font-weight: bold;'>Your ideal daily calorie intake should be 2000 Cal</span>", unsafe_allow_html=True)
+    st.markdown(f"<span style='color: blue;font-size: 18px;font-weight: bold;'>Your ideal daily water intake should be 2.7 L</span>", unsafe_allow_html=True)
+elif (gender == 'Other'):
+    st.markdown(f"<span style='color: blue;font-size: 18px;font-weight: bold;'>Sorry, info not available :)</span>", unsafe_allow_html=True)
+else:
+    st.markdown(f"<span style='color: blue;font-size: 18px;font-weight: bold;'>Sorry, info not available :)</span>", unsafe_allow_html=True)
+    
